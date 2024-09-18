ed062a8057888112f704242a5c65a1b418f5668d
Lunaji <lunacyjay@gmail.com>
2024-09-18 19:23:27 +0200

    Parametrize test

--- tests/test_xbf_load_save.py
+++ tests/test_xbf_load_save.py
@@ -5,17 +5,11 @@ import filecmp
 import pytest
 
 #@pytest.mark.file_test
-def test_xbf_load_save_verification():
+@pytest.mark.parametrize('xbf_file', [f for f in Path('Data/').rglob('*') if f.is_file() and f.suffix.lower() == '.xbf'])
+def test_xbf_load_save_verification(xbf_file):
     # if not pytestconfig.getoption('file_test'):
     #     pytest.skip('Skipping file tests')
-
-    base_path = Path('Data/')
-    suffix = '.xbf'
-
-    xbf_files = [f for f in base_path.rglob('*') if f.is_file() and f.suffix.lower() == suffix]
-
-    for xbf_file in xbf_files:
-        scene = load_xbf(xbf_file.resolve())
-        with tempfile.NamedTemporaryFile(delete=True) as saved_file:
-            save_xbf(scene, saved_file.name)
-            assert filecmp.cmp(xbf_file, saved_file.name, shallow=False), f'Mismatch in file: {xbf_file}'
+    scene = load_xbf(xbf_file.resolve())
+    with tempfile.NamedTemporaryFile(delete=True) as saved_file:
+        save_xbf(scene, saved_file.name)
+        assert filecmp.cmp(xbf_file, saved_file.name, shallow=False)
