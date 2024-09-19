2487f622cc429277abbd0d29d3e71dc57c7faba9
Lunaji <lunacyjay@gmail.com>
2024-09-19 09:10:55 +0200

    Test writing vertex animation

--- tests/test_xbf_save.py
+++ tests/test_xbf_save.py
@@ -1,11 +1,17 @@
 import io
 import pytest
 from xanlib.xbf_save import (
-    write_Int32sl
+    write_Int32sl,
+    write_vertex_animation,
 )
 
 def test_write_Int32sl(pos_int):
     buffer = io.BytesIO()
     write_Int32sl(buffer, pos_int['decoded'])
     assert buffer.getvalue() == pos_int['binary']
+
+def test_write_vertex_animation(vertex_animation):
+    buffer = io.BytesIO()
+    write_vertex_animation(buffer, vertex_animation['decoded'])
+    assert buffer.getvalue() == vertex_animation['binary']
 
