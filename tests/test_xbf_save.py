417e2f7baa5f61d198b087a7f43286c1fbc5a6a9
Lunaji <lunacyjay@gmail.com>
2024-09-20 10:23:22 +0200

    Make buffer a fixture

--- tests/test_xbf_save.py
+++ tests/test_xbf_save.py
@@ -6,19 +6,20 @@ from xanlib.xbf_save import (
     write_vertex_animation,
 )
 
-def test_write_Int32sl(pos_int):
-    buffer = io.BytesIO()
+@pytest.fixture()
+def buffer():
+    return io.BytesIO()
+
+def test_write_Int32sl(buffer, pos_int):
     write_Int32sl(buffer, pos_int['decoded'])
     assert buffer.getvalue() == pos_int['binary']
 
-def test_write_vetex(vertex_data):
-    buffer = io.BytesIO()
+def test_write_vetex(buffer, vertex_data):
     expected, given = vertex_data
     write_vertex(buffer, given)
     assert buffer.getvalue() == expected
 
-def test_write_vertex_animation(vertex_animation):
-    buffer = io.BytesIO()
+def test_write_vertex_animation(buffer, vertex_animation):
     write_vertex_animation(buffer, vertex_animation['decoded'])
     assert buffer.getvalue() == vertex_animation['binary']
 
