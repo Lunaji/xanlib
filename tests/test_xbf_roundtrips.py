8d969f86f0ef068e1f0e869c48ed40b31f2f10b3
Lunaji <lunacyjay@gmail.com>
2024-09-28 12:02:31 +0200

    Rename test

--- tests/test_xbf_roundtrips.py
+++ tests/test_xbf_roundtrips.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.mark.slow
 @pytest.mark.parametrize('xbf_file', [f for f in Path('Data/').rglob('*') if f.is_file() and f.suffix.lower() == '.xbf'])
-def test_xbf_load_save_verification(xbf_file):
+def test_xbf_full_roundtrip(xbf_file):
 
     scene = load_xbf(xbf_file.resolve())
     with tempfile.NamedTemporaryFile(delete=True) as saved_file:
