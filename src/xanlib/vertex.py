67fd896d92ba91ee64cd3b9d4ef0bc2a641d7799
Lunaji <lunacyjay@gmail.com>
2024-11-06 00:08:19 +0100

    Write a frombytes method to do the actual work

--- src/xanlib/vertex.py
+++ src/xanlib/vertex.py
@@ -1,4 +1,5 @@
 from typing import BinaryIO
+from collections.abc import Buffer
 from dataclasses import dataclass
 from xanlib.math_utils import Vector3
 from struct import Struct
@@ -11,10 +12,14 @@ class Vertex:
     _struct = Struct("<6f")
 
     @classmethod
-    def fromstream(cls, stream: BinaryIO) -> "Vertex":
-        buffer = stream.read(cls._struct.size)
+    def frombytes(cls, buffer: Buffer) -> "Vertex":
         coords = cls._struct.unpack(buffer)
         return cls(
             Vector3(*coords[:3]),
             Vector3(*coords[3:]),
         )
+
+    @classmethod
+    def fromstream(cls, stream: BinaryIO) -> "Vertex":
+        buffer = stream.read(cls._struct.size)
+        return cls.frombytes(buffer)
