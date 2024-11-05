f18dc2c532669b0668fc434c88bc9b04c3d43453
Lunaji <lunacyjay@gmail.com>
2024-11-05 15:25:26 +0100

    Add type hints to scene functions

--- src/xanlib/scene.py
+++ src/xanlib/scene.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterator
 from dataclasses import dataclass, field
 from os import PathLike
 import re
@@ -15,18 +16,18 @@ class Scene:
     unparsed: bytes | None = None
 
     @property
-    def textures(self):
+    def textures(self) -> list[str]:
         return [texture.decode('ascii') for texture in re.split(b'\x00\x00|\x00\x02', self.textureNameData) if texture]
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Node]:
         for node in self.nodes:
             yield from node
 
-    def __getitem__(self, name):
+    def __getitem__(self, name: str) -> Node:
         return next(node for node in self if node.name == name)
 
 
-def print_node_names(scene):
+def print_node_names(scene: Scene) -> None:
     for node in scene.nodes:
         traverse(
             node,
