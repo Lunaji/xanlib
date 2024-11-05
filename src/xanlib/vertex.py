e7fdb10d4dde29a836d49682ab0ddc7594ed4c0b
Lunaji <lunacyjay@gmail.com>
2024-11-06 00:03:03 +0100

    Use a class attr to hold the format struct

--- src/xanlib/vertex.py
+++ src/xanlib/vertex.py
@@ -1,17 +1,20 @@
 from typing import BinaryIO
 from dataclasses import dataclass
 from xanlib.math_utils import Vector3
-from struct import unpack
+from struct import Struct
 
 
 @dataclass
 class Vertex:
     position: Vector3
     normal: Vector3
+    _struct = Struct("<6f")
 
     @classmethod
     def fromstream(cls, stream: BinaryIO) -> "Vertex":
-        return Vertex(
-            Vector3(*unpack("<3f", stream.read(4 * 3))),
-            Vector3(*unpack("<3f", stream.read(4 * 3))),
+        buffer = stream.read(cls._struct.size)
+        coords = cls._struct.unpack(buffer)
+        return cls(
+            Vector3(*coords[:3]),
+            Vector3(*coords[3:]),
         )
