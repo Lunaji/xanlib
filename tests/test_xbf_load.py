c3e2b8763891db09407aab81a84d5fa484558715
Lunaji <lunacyjay@gmail.com>
2024-11-05 23:41:37 +0100

    Format tests with Black

--- tests/test_xbf_load.py
+++ tests/test_xbf_load.py
@@ -14,39 +14,46 @@ def test_convert_signed_5bit(signed_5bit):
     v, expected = signed_5bit
     assert convert_signed_5bit(v) == expected
 
+
 def test_read_vertex(vertex):
     stream = io.BytesIO(vertex.encoded)
     result = read_vertex(stream)
     assert result == vertex.decoded
-    
+
+
 def test_read_face(face):
     stream = io.BytesIO(face.encoded)
     result = read_face(stream)
     assert result == face.decoded
+
 
 def test_read_vertex_animation(vertex_animation):
     stream = io.BytesIO(vertex_animation.encoded)
     result = read_vertex_animation(stream)
     assert result == vertex_animation.decoded
 
+
 def test_read_key_animation(key_animation):
     stream = io.BytesIO(key_animation.encoded)
     result = read_key_animation(stream)
     assert result == key_animation.decoded
+
 
 def test_read_node_basic(node_basic):
     stream = io.BytesIO(node_basic.encoded)
     result = read_node(stream)
     assert result == node_basic.decoded
 
+
 def test_read_node_with_children(node_with_children):
     stream = io.BytesIO(node_with_children.encoded)
     result = read_node(stream)
-    result.children[0].parent = None #TODO: remove this line
+    result.children[0].parent = None  # TODO: remove this line
     assert result == node_with_children.decoded
 
+
 def test_load_xbf(mocker, scene):
-    mock_open = mocker.patch('builtins.open', mocker.mock_open(read_data=scene.encoded))
+    mock_open = mocker.patch("builtins.open", mocker.mock_open(read_data=scene.encoded))
     result = load_xbf(scene.decoded.file)
     assert result == scene.decoded
-    mock_open.assert_called_once_with(scene.decoded.file, 'rb')
+    mock_open.assert_called_once_with(scene.decoded.file, "rb")
--- tests/test_xbf_save.py
+++ tests/test_xbf_save.py
@@ -9,36 +9,44 @@ from xanlib.xbf_save import (
     save_xbf,
 )
 
+
 @pytest.fixture()
 def stream():
     return io.BytesIO()
+
 
 def test_write_vertex(stream, vertex):
     write_vertex(stream, vertex.decoded)
     assert stream.getvalue() == vertex.encoded
 
+
 def test_write_face(stream, face):
     write_face(stream, face.decoded)
     assert stream.getvalue() == face.encoded
+
 
 def test_write_vertex_animation(stream, vertex_animation):
     write_vertex_animation(stream, vertex_animation.decoded)
     assert stream.getvalue() == vertex_animation.encoded
 
+
 def test_write_key_animation(stream, key_animation):
     write_key_animation(stream, key_animation.decoded)
     assert stream.getvalue() == key_animation.encoded
+
 
 def test_write_node_basic(stream, node_basic):
     write_node(stream, node_basic.decoded)
     assert stream.getvalue() == node_basic.encoded
 
+
 def test_write_node_with_children(stream, node_with_children):
-    node_with_children.decoded.children[0].parent = None #TODO: remove this line
+    node_with_children.decoded.children[0].parent = None  # TODO: remove this line
     write_node(stream, node_with_children.decoded)
     assert stream.getvalue() == node_with_children.encoded
 
+
 def test_save_xbf(mocker, scene):
-    mock_open = mocker.patch('builtins.open', mocker.mock_open())
+    mock_open = mocker.patch("builtins.open", mocker.mock_open())
     save_xbf(scene.decoded, scene.decoded.file)
-    mock_open.assert_called_once_with(scene.decoded.file, 'wb')
+    mock_open.assert_called_once_with(scene.decoded.file, "wb")
