1a440b917e20ad90b932785c5c15b427aa3c9159
Lunaji <lunacyjay@gmail.com>
2024-11-07 03:30:57 +0100

    Separate reading scene from file opening

--- src/xanlib/scene.py
+++ src/xanlib/scene.py
@@ -1,7 +1,9 @@
+from typing import BinaryIO
 from collections.abc import Iterator
 from dataclasses import dataclass, field
 import re
 from xanlib.node import Node, traverse
+from struct import unpack, calcsize
 
 
 @dataclass
@@ -27,6 +29,29 @@ class Scene:
 
     def __getitem__(self, name: str) -> Node:
         return next(node for node in self if node.name == name)
+
+    @classmethod
+    def fromstream(cls, stream: BinaryIO) -> "Scene":
+        scene = Scene()
+        header_fmt = "<2i"
+        header_size = calcsize(header_fmt)
+        scene.version, fxdata_size = unpack(header_fmt, stream.read(header_size))
+        scene.FXData = stream.read(fxdata_size)
+        texture_data_size = unpack("<i", stream.read(4))[0]
+        scene.textureNameData = stream.read(texture_data_size)
+        while True:
+            try:
+                node = Node.fromstream(stream)
+                if node.transform is None:
+                    current_position = stream.tell()
+                    stream.seek(0, 2)
+                    assert current_position == stream.tell(), "Not at EOF"
+                    return scene
+                scene.nodes.append(node)
+            except Exception as e:
+                scene.error = e
+                scene.unparsed = stream.read()
+                return scene
 
 
 def print_node_names(scene: Scene) -> None:
--- src/xanlib/xbf_load.py
+++ src/xanlib/xbf_load.py
@@ -1,28 +1,7 @@
 from os import PathLike
-from struct import unpack, calcsize
-from xanlib.node import Node
 from xanlib.scene import Scene
 
 
 def load_xbf(filename: str | PathLike) -> Scene:
-    scene = Scene()
     with open(filename, "rb") as f:
-        header_fmt = "<2i"
-        header_size = calcsize(header_fmt)
-        scene.version, fxdata_size = unpack(header_fmt, f.read(header_size))
-        scene.FXData = f.read(fxdata_size)
-        texture_data_size = unpack("<i", f.read(4))[0]
-        scene.textureNameData = f.read(texture_data_size)
-        while True:
-            try:
-                node = Node.fromstream(f)
-                if node.transform is None:
-                    current_position = f.tell()
-                    f.seek(0, 2)
-                    assert current_position == f.tell(), "Not at EOF"
-                    return scene
-                scene.nodes.append(node)
-            except Exception as e:
-                scene.error = e
-                scene.unparsed = f.read()
-                return scene
+        return Scene.fromstream(f)
